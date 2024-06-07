f38363dbd81964f9c71f3d4cc1400ef81760508b
seoyoungwoo <ligerolibre@gmail.com>
2024-06-07 12:45:48 +0900

    Norton 인증 meta 추가

--- app.vue
+++ app.vue
@@ -17,6 +17,10 @@ useHead(() => ({
       name: 'naver-site-verification',
       content: '70ac51c1045ed1618cdebbd84d300a31d1b59c27'
     },
+    {
+      name: 'norton-safeweb-site-verification',
+      content: 'nsw-FU1NZGZGFFA-W5AHND4CPV79FIF9D0GOM6C8E9HWS1G6RKDUXFK3DI5717EKIKAPB7XOEYRTPER64X0TA6A1M9MJBOLAVKHVBYB1F5X97K2SVE5GHHUCK31DVZI1H615'
+    },
   ]
 }))
 </script>
